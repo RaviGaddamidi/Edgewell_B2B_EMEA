b0f3a84c1fb7ba75c68521c652438d1a8b9200b5
siddu1412 <siddaraju.nagappa@mindtree.com>
2015-05-05 17:58:21 +0530

    Configuration changes to sites in local.properties file

--- bin/custom/energizer/energizercore/src/com/energizer/core/setup/CoreSystemSetup.java
+++ bin/custom/energizer/energizercore/src/com/energizer/core/setup/CoreSystemSetup.java
@@ -51,8 +51,8 @@ public class CoreSystemSetup extends AbstractSystemSetup
 	public static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
 
 	//	public static final String ENERGIZER = "energizer";
-	public static final String PERSONAL_CARE = "personalCare";
-	public static final String HOUSEHOLD = "houseHold";
+	  public static final String PERSONAL_CARE = "personalCare";
+	  public static final String HOUSEHOLD = "houseHold";
 
 	@Resource
 	private ConfigurationService configurationService;
