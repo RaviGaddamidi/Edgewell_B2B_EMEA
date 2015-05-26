3b4c7c421c6b77836a40b59c730e3c8e67cf7143
energizerhybris <sriram.venkatasubramanian@mindtree.com>
2015-05-26 16:35:40 +0530

    Fixed the url decoding issue in opentext.

--- bin/custom/energizer/energizercore/src/com/energizer/core/invoice/EnergizerInvoiceService.java
+++ bin/custom/energizer/energizercore/src/com/energizer/core/invoice/EnergizerInvoiceService.java
@@ -5,12 +5,13 @@ package com.energizer.core.invoice;
 
 import de.hybris.platform.commercefacades.order.data.OrderData;
 
+
 /**
  * @author kaushik.ganguly
  * 
  */
 public interface EnergizerInvoiceService
 {
-	public byte[] getPDFInvoiceAsBytes(OrderData orderData);
+	public byte[] getPDFInvoiceAsBytes(OrderData orderData) throws Exception;
 
 }
