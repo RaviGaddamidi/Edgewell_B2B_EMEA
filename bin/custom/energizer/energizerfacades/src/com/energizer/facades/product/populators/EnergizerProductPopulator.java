5e9e608ae9bc5009483b567d990122797ec1a18a
AnithaVS <anitha.shastry@mindtree.com>
2015-05-28 17:18:30 +0530

    Fix for null CMIR record

--- bin/custom/energizer/energizerfacades/src/com/energizer/facades/product/populators/EnergizerProductPopulator.java
+++ bin/custom/energizer/energizerfacades/src/com/energizer/facades/product/populators/EnergizerProductPopulator.java
@@ -75,7 +75,6 @@ public class EnergizerProductPopulator implements Populator<EnergizerProductMode
 		productData.setDescription(energizerProductModel.getDescription() == null ? EMPTY : energizerProductModel.getDescription());
 		productData.setErpMaterialID(energizerProductModel.getCode() == null ? EMPTY : energizerProductModel.getCode());
 
-
 		//Setting the segment,family , group
 		productData.setSegmentName(energizerProductModel.getSegmentCode() == null ? EMPTY : energizerProductModel.getSegmentCode());
 		productData.setFamilyName(energizerProductModel.getFamilyCode() == null ? EMPTY : energizerProductModel.getFamilyCode());
