f3209fd4a400a55fbccdfdd2cedc471c26d4c5f8
siddu1412 <siddaraju.n@mindtree.com>
2015-07-08 16:21:30 +0530

    Added for CR Changes

--- bin/custom/energizer/energizerstorefront/web/src/com/energizer/storefront/forms/ForgottenPwdForm.java
+++ bin/custom/energizer/energizerstorefront/web/src/com/energizer/storefront/forms/ForgottenPwdForm.java
@@ -17,6 +17,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
 
 /**
@@ -25,6 +26,8 @@ import org.hibernate.validator.constraints.Email;
 public class ForgottenPwdForm
 {
 	private String email;
+	private String passwordQuestion;
+	private String passwordAnswer;
 
 	/**
 	 * @return the email
@@ -45,4 +48,34 @@ public class ForgottenPwdForm
 	{
 		this.email = email;
 	}
+
+
+	@NotNull(message = "{profile.passwordQuestion.invalid}")
+	@Size(min = 1, max = 255, message = "{profile.passwordQuestion.invalid}")
+	public String getPasswordQuestion()
+	{
+		return passwordQuestion;
+	}
+
+
+	public void setPasswordQuestion(final String passwordQuestion)
+	{
+		this.passwordQuestion = passwordQuestion;
+	}
+
+	/* Password Answer is restricted to max 25 length */
+	@NotNull(message = "{profile.passwordAnswer.invalid}")
+	@Size(min = 1, max = 25, message = "{profile.passwordAnswer.invalid}")
+	@NotBlank(message = "{profile.passwordAnswer.invalid}")
+	public String getPasswordAnswer()
+	{
+		return passwordAnswer;
+	}
+
+
+	public void setPasswordAnswer(final String passwordAnswer)
+	{
+		this.passwordAnswer = passwordAnswer;
+	}
+
 }
