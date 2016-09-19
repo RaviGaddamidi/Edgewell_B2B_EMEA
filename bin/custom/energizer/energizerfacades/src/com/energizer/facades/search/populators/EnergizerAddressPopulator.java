4735879650be5b2bc9a1b45bb9b909e717e2738c
shweta <shweta.sharma@mindtree.com>
2016-09-19 10:11:10 +0530

    changes made in facade data

--- bin/custom/energizer/energizerfacades/src/com/energizer/facades/search/populators/EnergizerAddressPopulator.java
+++ bin/custom/energizer/energizerfacades/src/com/energizer/facades/search/populators/EnergizerAddressPopulator.java
@@ -3,14 +3,20 @@ package com.energizer.facades.search.populators;
 import de.hybris.platform.commercefacades.user.converters.populator.AddressPopulator;
 import de.hybris.platform.commercefacades.user.data.AddressData;
 import de.hybris.platform.core.model.user.AddressModel;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * @author M1023097
- * 
+ *
  */
 public class EnergizerAddressPopulator extends AddressPopulator
 {
+	private Converter<AddressModel, AddressData> energizerAddressConverter;
+
 	@Override
 	public void populate(final AddressModel source, final AddressData target)
 	{
@@ -31,5 +37,40 @@ public class EnergizerAddressPopulator extends AddressPopulator
 		{
 			target.setActive(source.getActive());
 		}
+
+		target.setSoldTo(source.getSoldTo() != null ? source.getSoldTo() : false);
+
+		if (source.getShipToPartyList() != null)
+		{
+			List<AddressModel> shipToAddress = new ArrayList<AddressModel>();
+			final List<AddressData> shipToPartyListData = new ArrayList<AddressData>();
+			shipToAddress = source.getShipToPartyList();
+			for (final AddressModel address : shipToAddress)
+			{
+				shipToPartyListData.add(getEnergizerAddressConverter().convert(address));
+			}
+			if (shipToPartyListData != null)
+			{
+				target.setShipToPartyList(shipToPartyListData);
+			}
+		}
 	}
+
+	/**
+	 * @return the energizerAddressConverter
+	 */
+	public Converter<AddressModel, AddressData> getEnergizerAddressConverter()
+	{
+		return energizerAddressConverter;
+	}
+
+	/**
+	 * @param energizerAddressConverter
+	 *           the energizerAddressConverter to set
+	 */
+	public void setEnergizerAddressConverter(final Converter<AddressModel, AddressData> energizerAddressConverter)
+	{
+		this.energizerAddressConverter = energizerAddressConverter;
+	}
+
 }
